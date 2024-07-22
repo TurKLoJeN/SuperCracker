8cfc7148d50ba2b5436872a4e370075ccdf966b6
Oğuzhan Sarıoğlugil <32311900+TurKLoJeN@users.noreply.github.com>
2024-07-22 12:10:34 +0300

    Update SuperCracker.py

--- SuperCracker.py
+++ SuperCracker.py
@@ -3,7 +3,6 @@ import rarfile
 import os
 import time
 
-# ANSI escape codes
 GREEN = '\033[92m'
 RED = '\033[91m'
 BOLD_RED = '\033[1;91m'
@@ -11,7 +10,6 @@ PINK = '\033[95m'
 BOLD = '\033[1m'
 RESET = '\033[0m'
 
-# Messages dictionary for English and Turkish
 MESSAGES = {
     'EN': {
         'char_sets': 'Enter character sets (a: lowercase, A: uppercase, 1: digits, ?: special characters): ',
@@ -54,7 +52,6 @@ MESSAGES = {
 }
 
 def display_banner():
-    """Displays the banner"""
     banner = """
 :'######::'##::::'##:'########::'########:'########::'######::'########:::::'###:::::'######::'##:::'##:'########:'########:::::::::::::::::'##::::'##::::'##::::::::::'#####:::
 '##... ##: ##:::: ##: ##.... ##: ##.....:: ##.... ##:'##... ##: ##.... ##:::'## ##:::'##... ##: ##::'##:: ##.....:: ##.... ##:::::::::::::::: ##:::: ##::'####:::::::::'##.. ##::
@@ -72,7 +69,6 @@ def display_banner():
     print(f"{BOLD_RED}{banner}{RESET}")
 
 def get_language():
-    """Prompts the user to select a language"""
     while True:
         choice = input(f"{GREEN}Select language (1 - EN, 2 - TR): {RESET}").strip()
         if choice == '1':
@@ -83,23 +79,19 @@ def get_language():
             print(f"{RED}Invalid choice. Please enter 1 for English or 2 for Turkish.{RESET}")
 
 def format_time(seconds):
-    """Converts seconds to HH:MM:SS format"""
     hours = int(seconds // 3600)
     minutes = int((seconds % 3600) // 60)
     seconds = int(seconds % 60)
     return f"{hours:02}:{minutes:02}:{seconds:02}"
 
 def format_number(number):
-    """Formats numbers with thousands separator"""
     return "{:,}".format(number).replace(',', '.')
 
 def load_passwords(file_path):
-    """Loads passwords from a file"""
     with open(file_path, 'r') as file:
         return [line.strip() for line in file]
 
 def generate_passwords(characters, min_length, max_length, start_time):
-    """Generates passwords based on the given characters and length"""
     from itertools import product
     
     all_chars = {
@@ -125,7 +117,6 @@ def generate_passwords(characters, min_length, max_length, start_time):
                 file.write(''.join(password) + '\n')
                 count += 1
                 
-                # Calculate progress and estimated time
                 elapsed_time = time.time() - start_time
                 progress = count / total_combinations * 100
                 if count == 0:
@@ -133,12 +124,10 @@ def generate_passwords(characters, min_length, max_length, start_time):
                 else:
                     estimated_time = (elapsed_time / count) * (total_combinations - count)
                 
-                # Update the same line in the terminal
                 print(f"\r{GREEN}{MESSAGES[language]['progress']}{BOLD_RED}{format_number(count)}{RESET}/{BOLD_RED}{format_number(total_combinations)}{RESET} - {GREEN}{MESSAGES[language]['estimated_time_left']}{BOLD_RED}{format_time(estimated_time)}{RESET}", end='')
-        print()  # Move to next line after completion
+        print()
 
 def crack_zip(zip_file_path, passwords):
-    """Attempts to crack ZIP file"""
     try:
         with pyzipper.AESZipFile(zip_file_path, 'r') as zip_file:
             total_passwords = len(passwords)
@@ -157,93 +146,64 @@ def crack_zip(zip_file_path, passwords):
                 except (RuntimeError, pyzipper.BadZipFile, pyzipper.LargeZipFile):
                     pass
                 
-                # Calculate progress and estimated time
                 progress = (index + 1) / total_passwords * 100
                 elapsed_time = time.time() - start_time
                 estimated_time = (elapsed_time / (index + 1)) * (total_passwords - (index + 1))
                 
-                # Update the same line in the terminal
-                print(f"\r{GREEN}{MESSAGES[language]['progress']}{BOLD_RED}{progress:.2f}%{RESET} - {GREEN}{MESSAGES[language]['estimated_time_left']}{BOLD_RED}{format_time(estimated_time)}{RESET} - {GREEN}{MESSAGES[language]['password']}{BOLD}{index + 1}{RESET}{MESSAGES[language]['of']}{BOLD}{total_passwords}{RESET}", end='')
-
+                print(f"\r{GREEN}{MESSAGES[language]['progress']}{BOLD_RED}{progress:.2f}%{RESET} - {GREEN}{MESSAGES[language]['estimated_time_left']}{BOLD_RED}{format_time(estimated_time)}{RESET}", end='')
     except FileNotFoundError:
-        print(f"\n{RED}{MESSAGES[language]['unsupported_file_format']}: {zip_file_path}{RESET}")
-    print(f"\n{RED}{MESSAGES[language]['not_found']}{RESET}")
+        print(RED + MESSAGES[language]['invalid_file_path'] + RESET)
 
 def crack_rar(rar_file_path, passwords):
-    """Attempts to crack RAR file"""
     try:
-        rar_file = rarfile.RarFile(rar_file_path)
-        total_passwords = len(passwords)
-        start_time = time.time()
+        with rarfile.RarFile(rar_file_path) as rar_file:
+            total_passwords = len(passwords)
+            start_time = time.time()
 
-        print(f"\n{GREEN}Total passwords to try: {BOLD_RED}{format_number(total_passwords)}{RESET}")
+            print(f"\n{GREEN}Total passwords to try: {BOLD_RED}{format_number(total_passwords)}{RESET}")
 
-        for index, password in enumerate(passwords):
-            try:
-                rar_file.extractall(pwd=password.encode())
+            for index, password in enumerate(passwords):
+                try:
+                    rar_file.extractall(pwd=password)
+                    elapsed_time = time.time() - start_time
+                    print(f"\n{GREEN}{MESSAGES[language]['rar_password_found']}{BOLD_RED}{password}{RESET}")
+                    print(f"{PINK}Found in {BOLD}{int(elapsed_time // 60)}{RESET} minutes and {BOLD}{int(elapsed_time % 60)}{RESET} seconds")
+                    print(f"{PINK}Password was at position {BOLD}{index + 1}{RESET} in the list{RESET}")
+                    return
+                except rarfile.BadRarFile:
+                    pass
+                except rarfile.Error:
+                    pass
+                
+                progress = (index + 1) / total_passwords * 100
                 elapsed_time = time.time() - start_time
-                print(f"\n{GREEN}{MESSAGES[language]['rar_password_found']}{BOLD_RED}{password}{RESET}")
-                print(f"{PINK}Found in {BOLD}{int(elapsed_time // 60)}{RESET} minutes and {BOLD}{int(elapsed_time % 60)}{RESET} seconds")
-                print(f"{PINK}Password was at position {BOLD}{index + 1}{RESET} in the list{RESET}")
-                return
-            except (rarfile.BadRarFile, rarfile.PasswordRequired):
-                pass
-            
-            # Calculate progress and estimated time
-            progress = (index + 1) / total_passwords * 100
-            elapsed_time = time.time() - start_time
-            estimated_time = (elapsed_time / (index + 1)) * (total_passwords - (index + 1))
-            
-            # Update the same line in the terminal
-            print(f"\r{GREEN}{MESSAGES[language]['progress']}{BOLD_RED}{progress:.2f}%{RESET} - {GREEN}{MESSAGES[language]['estimated_time_left']}{BOLD_RED}{format_time(estimated_time)}{RESET} - {GREEN}{MESSAGES[language]['password']}{BOLD}{index + 1}{RESET}{MESSAGES[language]['of']}{BOLD}{total_passwords}{RESET}", end='')
-
+                estimated_time = (elapsed_time / (index + 1)) * (total_passwords - (index + 1))
+                
+                print(f"\r{GREEN}{MESSAGES[language]['progress']}{BOLD_RED}{progress:.2f}%{RESET} - {GREEN}{MESSAGES[language]['estimated_time_left']}{BOLD_RED}{format_time(estimated_time)}{RESET}", end='')
     except FileNotFoundError:
-        print(f"\n{RED}{MESSAGES[language]['unsupported_file_format']}: {rar_file_path}{RESET}")
-    print(f"\n{RED}{MESSAGES[language]['not_found']}{RESET}")
+        print(RED + MESSAGES[language]['invalid_file_path'] + RESET)
 
-def crack_file(file_path, password_file):
-    """Calls the appropriate cracking function based on file type"""
-    passwords = load_passwords(password_file)
-    
-    if file_path.lower().endswith('.zip'):
+display_banner()
+language = get_language()
+
+char_sets = input(GREEN + MESSAGES[language]['char_sets'] + RESET).strip()
+min_length = int(input(GREEN + MESSAGES[language]['min_length'] + RESET).strip())
+max_length = int(input(GREEN + MESSAGES[language]['max_length'] + RESET).strip())
+
+start_time = time.time()
+generate_passwords(char_sets, min_length, max_length, start_time)
+print(GREEN + MESSAGES[language]['passwords_saved'] + RESET)
+
+file_path = input(GREEN + MESSAGES[language]['file_path'] + RESET).strip()
+print(GREEN + MESSAGES[language]['file_path_entered'] + file_path + RESET)
+
+if os.path.exists('passlist.txt'):
+    passwords = load_passwords('passlist.txt')
+    if file_path.endswith('.zip'):
         crack_zip(file_path, passwords)
-    elif file_path.lower().endswith('.rar'):
+    elif file_path.endswith('.rar'):
         crack_rar(file_path, passwords)
     else:
-        print(f"{RED}{MESSAGES[language]['unsupported_file_format']}{RESET}")
-
-def main():
-    """Prompts the user for the file path and password list, and starts the cracking process"""
-    display_banner()  # Display the banner
-    
-    # Get language choice
-    global language
-    language = get_language()
-    
-    # Prompt for password generation if needed
-    char_sets = input(f"{GREEN}{MESSAGES[language]['char_sets']}{RESET}").strip()
-    min_length = int(input(f"{GREEN}{MESSAGES[language]['min_length']}{RESET}"))
-    max_length = int(input(f"{GREEN}{MESSAGES[language]['max_length']}{RESET}"))
-    
-    start_time = time.time()
-    generate_passwords(char_sets, min_length, max_length, start_time)
-    
-    # Prompt for file path
-    print()  # Add a new line for separation
-    file_path = input(f"{GREEN}{MESSAGES[language]['file_path']}{RESET}").strip()
-    print(f"{BOLD_RED}{MESSAGES[language]['file_path_entered']}{file_path}{RESET}")
-    password_file = 'passlist.txt'
-    
-    if not os.path.isfile(file_path):
-        print(f"{RED}{MESSAGES[language]['invalid_file_path']}{RESET}")
-        return
-
-    if not os.path.isfile(password_file):
-        print(f"{RED}{MESSAGES[language]['password_list_not_found']}{RESET}")
-        return
-
-    crack_file(file_path, password_file)
-
-# Run the program
-if __name__ == "__main__":
-    main()
+        print(RED + MESSAGES[language]['unsupported_file_format'] + RESET)
+else:
+    print(RED + MESSAGES[language]['password_list_not_found'] + RESET)
